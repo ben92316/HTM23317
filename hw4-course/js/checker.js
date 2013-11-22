79edd1e968a6e23b80ac120c65aba66b668b4cd4
Ben <ben92316@gmail.com>
2013-11-22 21:42:53 +0800

    2

--- hw4-course/js/checker.js
+++ hw4-course/js/checker.js
@@ -60,4 +60,4 @@ results.after('<div class="hw4-complete alert alert-info">掃描完成</div>');
   // 拿到使用者 group 列表的 response 之後：
   // results.after('<div class="hw4-complete alert alert-info">掃描完成</div>');
 
-});
+};
